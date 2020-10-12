fb6eaae36377d6eb131c1da40eafde7bde58f1e1
unsxxndxrk <selincorvin@yahoo.com>
2020-10-12 16:35:23 +0300

    ref

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
-import Item from './components/Item';
+import React from "react";
+import "./App.css";
+import Item from "./components/Item";
 
 export default function App() {
-    return (
-        <div className="App">
-            <Item title={'block-1'} />
-            <Item title={'bock-2'} />
-        </div>
-    );
-};
+  return (
+    <div className="App">
+      <Item title={"block-1"} />
+      <Item title={"bock-2"} />
+    </div>
+  );
+}
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,49 +1,51 @@
-import React from 'react';
+import React from "react";
 
 export default class Item extends React.Component {
-    state = {
-        x: 0,
-        y: 0,
-        position: {
-            left: -50,
-            top: -50
-        }
-    };
+  state = {
+    x: 0,
+    y: 0,
+    position: {
+      left: -50,
+      top: -50,
+    },
+  };
 
-    parallax = (e) => {
-        var self = e.target.getBoundingClientRect();
+  parallax = (e) => {
+    var self = e.target.getBoundingClientRect();
 
-        this.setState({
-            x: (e.clientX - self.left).toFixed(1),
-            y: (e.clientY - self.top).toFixed(1),
-            position: {
-                left: -this.state.x / 5,
-                top: -this.state.y / 5
-            }
-        });
-    };
+    this.setState({
+      x: (e.clientX - self.left).toFixed(1),
+      y: (e.clientY - self.top).toFixed(1),
+      position: {
+        left: -this.state.x / 5,
+        top: -this.state.y / 5,
+      },
+    });
+  };
 
-    dropParallax = () => {
-        this.setState({
-            position: {
-                left: -50,
-                top: -50
-            }
-        });
-    };
+  dropParallax = () => {
+    this.setState({
+      position: {
+        left: -50,
+        top: -50,
+      },
+    });
+  };
 
-    render () {
-        return (
-            <div className="item-wrapper">
-                <div
-                    style={{left: this.state.position.left, top: this.state.position.top}}
-                    onMouseMove={this.parallax}
-                    onMouseLeave={this.dropParallax}
-                    title={this.props.title}
-                    className="item"
-                >
-                </div>
-            </div>
-        )
-    };
-};
+  render() {
+    return (
+      <div className="item-wrapper">
+        <div
+          style={{
+            left: this.state.position.left,
+            top: this.state.position.top,
+          }}
+          onMouseMove={this.parallax}
+          onMouseLeave={this.dropParallax}
+          title={this.props.title}
+          className="item"
+        />
+      </div>
+    );
+  }
+}
